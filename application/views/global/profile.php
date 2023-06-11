54bd627a603b660e714fe7de251019a98c212992
E41202233_FASTA BIQUL HOIROT <fastabikul87@gmail.com>
2023-06-11 22:57:15 +0700

    main

---
+++ application/views/global/profile.php
@@ -0,0 +1,71 @@
+<div class="row">
+                <div class="col-md-12">
+                   <center> <h4 class="page-head-line">Profile APLIKASI PENDIDIKAN</h4></center>
+                </div>
+
+<div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <i class=" glyphicon glyphicon-picture"></i>
+                            Photo Profile SDN 02 SUKOWONO
+                        </div>
+                                <div class="panel-body">
+
+                               <img src="<?php echo base_url(); ?>assets/2.webp" width="265" height="240">&nbsp
+                               <img src="<?php echo base_url(); ?>assets/3.webp" width="265" height="240">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-home"></i>
+                            Identitas Sekolah
+                        </div>
+                                <div class="panel-body">
+                                <table width="100%">
+                                <tr>
+                                    <td width="60%">
+                              
+                                        <b>Alamat : </b>Jl. Pahlawan NO.04 kecamatan Sukowono Kabupaten Jember Provinsi Jawa Timur<br>
+                                        <b>Telepon : </b>-<br>
+                                        <b>Email : </b>sdn02sukowono.jember@yahoo.com</br>
+                                        <b>NPSN : </b> - <br>
+                                        <b>Akreditasi :</b> B <br>
+                                     
+                                    </td>
+                                 </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-th-list"></i>
+                            VISI & MISI APLIKASI PENDIDIKAN 
+                        </div>
+                                <div class="panel-body">
+                                <ul align="justify"><b>Visi:</b> Mewujudkan kondusifitas untuk mendidik siswa menjadi beriman, bertakwa, berakhlak mulia, cerdas, mandiri dan bermanfaat.<br>
+                                <b>Misi:</b>  Refungsionalisasi, revitalisasi, reorientasi segenap sumber daya serta pemberdayaan stakeholder berdasarkan prinsip perbaikan terus menerus (continuous improvement) menuju sekolah yang unggul dan kompetitif.
+
+                               </ul>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            
+        </div>
+    </div>
